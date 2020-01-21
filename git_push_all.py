6410e9ed555b8ff089b711635462d195c08ea4da
Gautham Kolluru <sakollur@cisco.com>
2020-01-21 12:09:46 +0530

    modified git_push_all.py

--- git_push_all.py
+++ git_push_all.py
@@ -26,17 +26,19 @@ class GitPullPush:
 
     def git_push(self):
         self.dir_list = self.find_local_repos()
-        self.git_pull()
+        # self.git_pull()
         for directory in self.dir_list:
+            subprocess.run(['git', 'pull'], cwd=os.path.join(
+                self.set_root_dir(), directory), shell=True)
             subprocess.run(['git', 'add', '.'], cwd=os.path.join(
-                self.set_root_dir(), directory), shell=True, capture_output=True)
+                self.set_root_dir(), directory), shell=True)
             git_status = subprocess.run(['git', 'status'], cwd=os.path.join(
                 self.set_root_dir(), directory), shell=True, capture_output=True)
             if self.nothing_to_commit not in git_status.stdout:
                 print(git_status.stdout)
                 self.commit_message = input("Enter Commit Message: ")
                 subprocess.run(['git', 'commit', '-m', self.commit_message], cwd=os.path.join(
-                    self.set_root_dir(), directory), shell=True, capture_output=True)
+                    self.set_root_dir(), directory), shell=True)
                 git_push_result = subprocess.run(['git', 'push'], cwd=os.path.join(
                     self.set_root_dir(), directory), shell=True, capture_output=True)
         return git_push_result.stdout
@@ -45,5 +47,4 @@ class GitPullPush:
 if __name__ == "__main__":
     git_obj = GitPullPush()
     # git_obj.git_pull()
-    git_push_output = git_obj.git_push()
-    print(git_push_output)
+    git_obj.git_push()
